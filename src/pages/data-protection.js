2f77c0a1df39da3b6c0c5d69eaa4c9275a544be9
Darja Sergejcuk <you@example.com>
2019-05-10 23:57:42 +0200

    step2

---
+++ src/pages/data-protection.js
@@ -0,0 +1,18 @@
+import React from "react"
+import SEO from "../components/SEO"
+import Layout from "../components/layout"
+
+const DataProtectionPage = () => (
+  <Layout>
+    <SEO title="Data Protection" />
+
+    <h1>Data Protection</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima quisquam
+      voluptatem hic, aliquam alias magnam id ipsa corrupti enim praesentium vel
+      tempora ducimus quos quam illum nihil officia non numquam.
+    </p>
+  </Layout>
+)
+
+export default DataProtectionPage
---
+++ src/pages/imprint.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const ImprintPage = () => (
+  <Layout>
+    <SEO title="Imprint" />
+    <h1>Imprint</h1>
+    <h2>Responsible</h2>
+    Darja Sergejcuk
+    <h2>Credits</h2>
+    ...
+  </Layout>
+)
+
+export default ImprintPage
